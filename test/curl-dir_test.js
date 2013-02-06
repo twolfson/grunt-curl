260a2ca390831401e792496d39a2ef94be995e91
Todd Wolfson <todd@twolfson.com>
2013-02-06 01:00:27 -0800

    Router test added and passing

--- test/curl-dir_test.js
+++ test/curl-dir_test.js
@@ -51,5 +51,18 @@ exports['curl-dir'] = {
     test.equal(actualLabContent, expectedLabContent, 'should return the correct value for cookiejar.js.');
 
     test.done();
+  },
+  'router': function(test) {
+    test.expect(2);
+    // tests here
+    var expectedLabContent = grunt.file.read('expected/file.js'),
+        actualLabContent = grunt.file.read('actual/router/ajax/libs/labjs/2.0.3/LAB.min.js');
+    test.equal(actualLabContent, expectedLabContent, 'should return the correct value for LAB.min.js.');
+
+    var expectedCookiejarContent = grunt.file.read('expected/cookiejar.js'),
+        actualCookiejarContent = grunt.file.read('actual/router/ajax/libs/cookiejar/0.5/cookiejar.js');
+    test.equal(actualLabContent, expectedLabContent, 'should return the correct value for cookiejar.js.');
+
+    test.done();
   }
 };
