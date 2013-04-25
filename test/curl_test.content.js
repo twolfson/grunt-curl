4fae73a9eb2885c2e3067aa615d1cfd747d3839f
Todd Wolfson <todd@twolfson.com>
2013-04-24 23:31:25 -0700

    Filling out content for tests

--- test/curl_test.content.js
+++ test/curl_test.content.js
@@ -3,40 +3,48 @@ var chai = require('chai'),
     expect = chai.expect;
 
 module.exports = {
+  // Grunt commands
   'grunt curl': function () {
-
+    this.cmd = 'grunt curl:';
   },
+  'grunt curl-dir': function () {
+    this.cmd = 'grunt curl-dir:';
+  },
+
+  // curl tasks
   'downloading a js (utf16) file': function () {
-
-  },
-  'is successful':  function () {
-
+    this.task = 'js';
+    // TODO: Add filename(s) here
   },
   'downloading a zip (binary) file':  function () {
-
+    this.task = 'zip';
   },
   'downloading a file from an invalid domain':  function () {
+    this.task = 'nonExistingDomain';
+  },
+  'downloading an non-existant file':  function () {
+    this.task = 'nonExistingFile';
+  },
+
+  // curl-dir tasks
+  'downloading multiple files': function () {
+    this.task = 'multi';
+  },
+  'downloading brace expanded files':  function () {
+    this.task = 'braceExpansion';
+  },
+  'using a custom router': function () {
+    this.task = 'router';
+  },
+
+  // curl and curl-dir results
+  'is successful':  function () {
 
   },
   'throws an error':  function () {
 
   },
   'does not create the file':  function () {
-
-  },
-  'downloading a file from an invalid domain':  function () {
-
-  },
-  'grunt curl-dir': function () {
-
-  },
-  'downloading multiple files': function () {
-
-  },
-  'downloading brace expanded files':  function () {
-
-  },
-  'using a custom router': function () {
 
   }
 };
