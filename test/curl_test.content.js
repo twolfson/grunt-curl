4b4ac695d4e4715a8496d2302894dfc13721fa39
Todd Wolfson <todd@twolfson.com>
2013-04-24 23:25:18 -0700

    Went through the tedium of creating a content file

---
+++ test/curl_test.content.js
@@ -0,0 +1,42 @@
+// Load in chai and expect
+var chai = require('chai'),
+    expect = chai.expect;
+
+module.exports = {
+  'grunt curl': function () {
+
+  },
+  'downloading a js (utf16) file': function () {
+
+  },
+  'is successful':  function () {
+
+  },
+  'downloading a zip (binary) file':  function () {
+
+  },
+  'downloading a file from an invalid domain':  function () {
+
+  },
+  'throws an error':  function () {
+
+  },
+  'does not create the file':  function () {
+
+  },
+  'downloading a file from an invalid domain':  function () {
+
+  },
+  'grunt curl-dir': function () {
+
+  },
+  'downloading multiple files': function () {
+
+  },
+  'downloading brace expanded files':  function () {
+
+  },
+  'using a custom router': function () {
+
+  }
+};
--- test/curl_test.outline.js
+++ test/curl_test.outline.js
@@ -29,4 +29,4 @@ module.exports = [{
       'is successful': true
     }
   }
-}]
+}];
